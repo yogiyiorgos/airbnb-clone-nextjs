2ce4eaff4db14df2bd4977556be3706cecf3fa0b
yogiyiorgos <georgefil@outlook.com>
2023-07-24 20:57:51 +0200

    AirClone01FIXEDRegisterJul24

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '@/pages/api/auth/[...nextAuth]'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import prisma from '@/app/libs/prismadb'
 
 export async function getSession() {
